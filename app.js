27a59dcd46648a7309adbfb4d9f325368ad1d7b7
binit <binitgupta.1711@gmail.com>
2025-09-17 02:54:45 +0530

    working ejs form

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const indexRouter = require('./routes/indexRouter');
 const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 
 app.use("/", indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,39 @@
 const { Router } = require('express');
+const express = require('express'); // Change this line
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+indexRouter.use(express.urlencoded({ extended: false })); // Now this works
+
 indexRouter.get('/', (req, res) => {
-    res.send('Welcome to the Mini Message Board API!');
+    res.render('index', {title: 'Mini Message Board', messages: messages});
 })
+indexRouter.get('/new', (req, res) => {
+    res.render('form', {title: 'New Message'});
+});
+indexRouter.post('/new', (req, res) => {
+    const user = req.body.user;
+    const text = req.body.text;
+    try {
+        messages.push({ text: text, user: user, added: new Date() });
+        console.log("Successfully added message");
+       res.redirect('/');
+    } catch (error) {
+        console.error("Error adding message:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = indexRouter;
